a8c2e3d562c5e08a2a2a7826383bb2cfeefbe64b
Olivier-Kango <olivierkango@gmail.com>
2023-03-16 10:59:14 +0200

    Fix some bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+ 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email role password])
+
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :password)
+    end
   end
 end
