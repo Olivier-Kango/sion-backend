c5685479548394284ca075f99a4709229097f166
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 22:01:12 +0300

    Fix WARNING

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,9 +6,10 @@ set -o errexit
 gem install bundler --user-install
 
 # Add the gem executables directory to the PATH
-export PATH=$HOME/.gem/ruby/3.1.3/bin:$PATH
+export PATH=$HOME/.gem/ruby/3.1.3/bin:$HOME/.gem/ruby/2.5.0/bin:$PATH
 
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
