0054990a8093e8f32684d328b8393a8d02f7cea9
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 21:17:32 +0300

    Update ruby version

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,10 @@
 # exit on error
 set -o errexit
 
-rbenv global 3.1.3
+# Add the gem executables directory to the PATH
+export PATH=/opt/render/ruby/3.1.3/bin:$PATH
 
 gem install bundler -v 2.4.10 --user-install
-
-# Add the gem executables directory to the PATH
-export PATH=$HOME/.gem/ruby/3.1.3/bin:$PATH
 
 bundle install
 bundle exec rake assets:precompile
