324d4fdea9ffc08c28a3848b009fb31c1177ab5b
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 22:02:43 +0300

    Initialization

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,14 +2,7 @@
 # exit on error
 set -o errexit
 
-# Install Bundler
-gem install bundler --user-install
-
-# Add the gem executables directory to the PATH
-export PATH=$HOME/.gem/ruby/3.1.3/bin:$HOME/.gem/ruby/2.5.0/bin:$PATH
-
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
-
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
