a754cf9ff6063878a594de722c6e145f4c9d3592
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 21:33:09 +0300

    Udpate bundler

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,11 +6,11 @@ set -o errexit
 ruby_version="3.1.3"
 
 # Add the gem executables directory to the PATH
-export PATH=/opt/render/ruby/2.5.0/bin:$PATH
+export PATH=/opt/render/ruby/3.1.3/bin:$PATH
 
-gem install bundler -v 2.2.28 --user-install
+gem install bundler -v 2.4.10 --user-install
 
-~/.gem/ruby/2.5.0/bin/bundle install
-~/.gem/ruby/2.5.0/bin/bundle exec rake assets:precompile
-~/.gem/ruby/2.5.0/bin/bundle exec rake assets:clean
-~/.gem/ruby/2.5.0/bin/bundle exec rake db:migrate
+~/.gem/ruby/3.1.3/bin/bundle install
+~/.gem/ruby/3.1.3/bin/bundle exec rake assets:precompile
+~/.gem/ruby/3.1.3/bin/bundle exec rake assets:clean
+~/.gem/ruby/3.1.3/bin/bundle exec rake db:migrate
