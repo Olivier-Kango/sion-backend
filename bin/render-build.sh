c4b601d21f4d4191b538cafe29fb9bf3a4915e26
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 21:35:25 +0300

    New file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,12 +5,18 @@ set -o errexit
 # Update Ruby version
 ruby_version="3.1.3"
 
-# Add the gem executables directory to the PATH
-export PATH=/opt/render/ruby/3.1.3/bin:$PATH
+# Install Ruby using RVM
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://get.rvm.io | bash -s stable --ruby=$ruby_version --gems=bundler
 
-gem install bundler -v 2.4.10 --user-install
+# Use the installed Ruby and Bundler
+source ~/.rvm/scripts/rvm
+rvm use $ruby_version
 
-~/.gem/ruby/3.1.3/bin/bundle install
-~/.gem/ruby/3.1.3/bin/bundle exec rake assets:precompile
-~/.gem/ruby/3.1.3/bin/bundle exec rake assets:clean
-~/.gem/ruby/3.1.3/bin/bundle exec rake db:migrate
+gem install bundler -v 2.4.10
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
