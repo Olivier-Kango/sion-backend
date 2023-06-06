ae6b0c11c4c95614953d6551402c6aa9e5cf375a
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 21:24:12 +0300

    Bundle version V 2.2.28

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,13 @@
 # exit on error
 set -o errexit
 
-gem install bundler -v 2.4.10 --user-install
-
 # Add the gem executables directory to the PATH
-export PATH=/opt/render/ruby/2.6.0/bin:$PATH
+export PATH=/opt/render/ruby/2.5.0/bin:$PATH
+
+gem install bundler -v 2.2.28 --user-install
 
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
