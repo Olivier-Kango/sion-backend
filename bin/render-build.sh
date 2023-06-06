b676b88053a0c390977512563e854a41442a4dda
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 20:59:28 +0300

    Add PATH

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 gem install bundler -v 2.1 --user-install
 
+# Add the gem executables directory to the PATH
+export PATH=$HOME/.gem/ruby/2.5.0/bin:$PATH
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
