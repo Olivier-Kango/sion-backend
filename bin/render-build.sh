3908ac2517a7e27a406760a6a2ad91f387cddff0
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 18:02:36 +0300

    Add rbenv local

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,22 +2,14 @@
 # exit on error
 set -o errexit
 
-# Upgrade RubyGems
-gem update --system 3.2.3
+# Set the Ruby version
+rbenv local 3.1.3
 
-# Specify custom gem installation directory
-export GEM_HOME="/tmp/vendor/bundle"
-export BUNDLE_PATH="${GEM_HOME}"
+# Install Bundler
+gem install bundler -v 2.4.10
 
-# Set the bundle path
-bundle config set --local path "${GEM_HOME}"
-
-gem install bundler -v 2.3.26
-
-# Add the directory containing the Bundler executable to the PATH
-export PATH="${GEM_HOME}/bin:${PATH}"
-
-bundle _2.3.26_ install
-bundle _2.3.26_ exec rails assets:precompile
-bundle _2.3.26_ exec rails assets:clean
-bundle _2.3.26_ exec rails db:migrate
+# Run bundle install and other commands
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
