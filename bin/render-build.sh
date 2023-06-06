011c56908c490e546d13d84277e82bb1c8427fe3
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 17:50:53 +0300

    Delete path

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,8 @@
 # exit on error
 set -o errexit
 
-# Specify a different writable directory
-export GEM_HOME="/tmp/vendor/bundle"
-
 gem install bundler -v 2.3.26
-bundle _2.3.26_ install --path "${GEM_HOME}"
+bundle _2.3.26_ install
 bundle _2.3.26_ exec rails assets:precompile
 bundle _2.3.26_ exec rails assets:clean
 bundle _2.3.26_ exec rails db:migrate
