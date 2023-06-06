4467baafaa610997718a861e6c37cfbca672d61d
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 21:05:27 +0300

    Update ruby Version to 3.1.3

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 gem install bundler -v 2.4.10 --user-install
 
 # Add the gem executables directory to the PATH
-export PATH=$HOME/.gem/ruby/2.5.0/bin:$PATH
+export PATH=$HOME/.gem/ruby/3.1.3/bin:$PATH
 
 bundle install
 bundle exec rake assets:precompile
