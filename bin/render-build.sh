0544b71644fcdd1579ea185b4e00f82455907e63
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 17:42:38 +0300

    update render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,10 @@
 # exit on error
 set -o errexit
 
+export GEM_HOME="/app/vendor/bundle"
+
 gem install bundler
-bundle install --path vendor/bundle
+bundle install --path "${GEM_HOME}"
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
