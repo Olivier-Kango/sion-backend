7007378064275b25b65e6b750aba38db129d8d4d
Olivier-Kango <olivierkango@gmail.com>
2023-06-06 21:43:00 +0300

    Update render.yml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,21 +2,7 @@
 # exit on error
 set -o errexit
 
-# Update Ruby version
-ruby_version="3.1.3"
-
-# Install Ruby using RVM
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -sSL https://get.rvm.io | bash -s stable --ruby=$ruby_version --gems=bundler
-
-# Use the installed Ruby and Bundler
-source ~/.rvm/scripts/rvm
-rvm use $ruby_version
-
-gem install bundler -v 2.4.10
-
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
