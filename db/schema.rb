a187d87de00b49d129472a407c9feff588729673
Olivier-Kango <olivierkango@gmail.com>
2023-07-11 21:30:37 +0300

    Add quantity and categiry columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_181149) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_182803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_181149) do
     t.integer "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|
