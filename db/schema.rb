601cc0f8840c36c181547fd8ce4ddb32d16c9cc2
Olivier-Kango <olivierkango@gmail.com>
2023-08-15 05:21:24 +0300

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_020844) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_021958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
