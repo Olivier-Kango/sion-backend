03fe4bd2aa0a362144d9f9dedb09ad091859db7f
Olivier-Kango <olivierkango@gmail.com>
2023-09-05 14:49:00 +0300

    remove migration

--- db/migrate/20230813165816_add_order_type_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderTypeToOrders < ActiveRecord::Migration[7.0]
-  def change
-    add_column :orders, :order_type, :string
-  end
-end
--- db/migrate/20230904123913_add_profile_image_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddProfileImageToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :profile_image, :string
-  end
-end
