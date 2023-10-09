9fff2ac7e850bcb18eab828c2989fd0b3e04e8c9
Olivier-Kango <olivierkango@gmail.com>
2023-10-09 09:44:42 +0300

    Add unit_purchase_price

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_many :users, through: :orders
 
   validates :name, :image, presence: true
+  validates :unit_purchase_price, presence: true, numericality: true, comparison: { greater_than: 0 }
   validates :unit_price, presence: true, numericality: true, comparison: { greater_than: 0 }
   validates :category, :subcategory, :image, presence: true
   validates :quantity, presence: true, numericality: true, comparison: { greater_than: 0 }
