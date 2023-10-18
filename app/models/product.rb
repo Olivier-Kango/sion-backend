e1e0a61ab37c8f7d4b841ddec07025f0a0c9f984
Olivier-Kango <olivierkango@gmail.com>
2023-10-18 10:49:52 +0300

    Add many stock movement in product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :users, through: :orders
+  has_many :stock_movements, dependent: :destroy
 
   validates :name, :image, presence: true
   validates :unit_purchase_price, presence: true, numericality: true, comparison: { greater_than: 0 }
