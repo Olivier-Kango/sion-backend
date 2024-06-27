a8b3792f748db58c8bb3d7a27ca15e4a99fb7c1b
Olivier-Kango <olivierkango@gmail.com>
2024-06-27 12:01:03 +0300

    update Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,11 +2,11 @@ class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user
 
-  validates :user_id, :product_id, presence: { message: 'User_id and Product_id should be not be empty' }
-  validates :quantity, presence: true, numericality: true, comparison: { greater_than: 0 }
+  validates :user_id, :product_id, presence: { message: 'User_id and Product_id should not be empty' }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :delivery_point, presence: true
 
-  enum status: { new: 'new', processing: 'processing', delivered: 'delivered', paid: 'paid' }
+  enum status: { new_order: 'new', processing: 'processing', delivered: 'delivered', paid: 'paid' }
 
   def price
     quantity * product.unit_price
