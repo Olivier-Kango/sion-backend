6c16a6d33aa3aee61a165f02c354d537da207c0b
Olivier-Kango <olivierkango@gmail.com>
2024-06-27 12:27:04 +0300

    Delete all Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(name:'Sion', email: 'olivierkango@gmail.com', role:'admin', password: 'Bleu111111', password_confirmation: 'password')
+User.where.not(email: 'olivierkango@gmail.com').destroy_all;
